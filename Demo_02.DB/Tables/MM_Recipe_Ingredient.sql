2e3f35101a372b5f5b89e8a9ab458ac4789cad9b
Aude Beurive <aude.beurive@bstorm.be>
2023-10-09 12:11:18 +0200

    add on delete cascade on fk recipe of MM_recipe_ingredient

--- Demo_02.DB/Tables/MM_Recipe_Ingredient.sql
+++ Demo_02.DB/Tables/MM_Recipe_Ingredient.sql
@@ -5,6 +5,7 @@
 	[Quantity] DECIMAL(5,2) NOT NULL,
 	[Unit] NVARCHAR(50) NOT NULL,
 	CONSTRAINT PK_Recipe_Ingredient PRIMARY KEY ([RecipeId], [IngredientId]),
-	CONSTRAINT FK_MM_Recipe FOREIGN KEY ([RecipeId]) REFERENCES [Recipe]([Id]),
+	CONSTRAINT FK_MM_Recipe FOREIGN KEY ([RecipeId]) REFERENCES [Recipe]([Id])
+	ON DELETE CASCADE,
 	CONSTRAINT FK_MM_Ingredient FOREIGN KEY ([IngredientId]) REFERENCES [Ingredient]([Id])
 )
